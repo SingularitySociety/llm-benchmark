2d161e6448755daaa09e297438f425cea4e5cfa5
isamu <isamu@to-kyo.to>
2023-05-23 17:24:36 +0900

    refactor gpt script

--- scripts/gpt.ts
+++ scripts/gpt.ts
@@ -1,5 +1,6 @@
 import * as fs from 'fs';
 import { Configuration, OpenAIApi, ChatCompletionRequestMessage } from "openai";
+import { main } from "./common";
 
 const gpt_token = process.env.GPT_API_KEY;
 
@@ -12,7 +13,7 @@ export const ask = async (
   messages: ChatCompletionRequestMessage[],
   model = "gpt-3.5-turbo-0301"
 ) => {
-  console.log(messages, model);
+  // console.log(messages, model);
   try {
     const response = await openai.createChatCompletion({
       model: model,
@@ -25,36 +26,6 @@ export const ask = async (
   } catch (e) {
     console.log(e);
     return null;
-  }
-};
-
-const main = async (resultModelKey: string, chat: (text: string) => Promise<string>) => {
-  const dataFile = __dirname + '/../data/data.json';
-  const resultFile = __dirname + '/../results/data.json';
-  
-  const dataSet = JSON.parse(fs.readFileSync(dataFile, 'utf8'));
-  const resultDataSet = (() => {
-    if (fs.existsSync(resultFile)) {
-      return JSON.parse(fs.readFileSync(resultFile, 'utf8'));
-    }
-    return [];
-  })();
-  
-  for await (const data of dataSet) {
-    const text = data.text.en || data.text.ja;
-    const result = await chat(text);
-    const matchKey = Object.keys(resultDataSet).find((res: string) => resultDataSet[res].id === data.id);
-    if (matchKey) {
-      const tmp = resultDataSet[matchKey];
-      tmp ["result"][resultModelKey] = result;
-    } else {
-      data["result"] = {};
-      data["result"][resultModelKey] = result;
-      resultDataSet.push(data);
-    }
-    
-    fs.writeFileSync(resultFile,  JSON.stringify(resultDataSet, null, '    '));
-    console.log(resultDataSet);
   }
 };
 
@@ -77,6 +48,6 @@ const gptChat35 = gptChat("gpt-3.5-turbo");
 
 main(resultModelKey, gptChat35);
 
-const resultModelKey = "gpt4";
-const gptChat35 = gptChat("gpt-4");
-main(resultModelKey, gptChat35);
+// const resultModelKey = "gpt4";
+// const gptChat35 = gptChat("gpt-4");
+// main(resultModelKey, gptChat35);
