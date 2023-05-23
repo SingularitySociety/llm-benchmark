fad777ae3d28875e6da062a7972b4f305160d91b
isamu <isamu@to-kyo.to>
2023-05-23 09:12:48 +0900

    add ja question

--- scripts/ToJson.rb
+++ scripts/ToJson.rb
@@ -1,13 +1,18 @@
 require "json"
 
-text = open(__dir__ + "/../text/en.txt").read
-
 ret = []
-text.split(/(\r|\n)+/).each do |line|
-  if line.strip != ""
-    ret.push({text: {en: line.strip}})
+
+def readFile(ret, file, locale) 
+  text = open(__dir__ + "/../text/" + file).read
+  text.split(/(\r|\n)+/).each do |line|
+    if line.strip != ""
+      ret.push({text: {locale => line.strip}})
+    end
   end
 end
+
+readFile(ret, "en.txt", "en")
+readFile(ret, "ja.txt", "ja")
 
 File.open(__dir__ + "/../data/data.json","w") do |f|
   f.write(JSON.pretty_generate(ret))
